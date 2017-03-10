43499de1a97892626ccfab2334c66ff3ee0c0623
CharlesGFYu <charlesgfyu@gmail.com>
2017-03-10 12:36:14 +0800

    20170310-1236

--- src/components/Main.js
+++ src/components/Main.js
@@ -25,8 +25,13 @@ class ImgFigure extends React.Component {
 		figureArr.push(this.figureItem);
 	}
 	render() {
+		let styleObj = {};
+		//如果 props 属性中指定了这张图片的位置，则使用
+		if(this.props.arrange.pos){
+			styleObj = this.props.arrange.pos;
+		}
 		return (
-			<figure className="img-figure" ref={(figureItem) => { this.figureItem = figureItem; }}>
+			<figure className="img-figure" style={styleObj} ref={(figureItem) => { this.figureItem = figureItem; }}>
 				<img src={this.props.data.imageURL} alt={this.props.data.title}/>
 				<figcaption>
 					<h2 className="img-title">{this.props.data.title}</h2>
@@ -66,11 +71,72 @@ class AppComponent extends React.Component {
 		}
 	}
 	/*
+		* 获取区间内的一个随机值
+	*/
+	getRangeRandom(low, high) {
+		return Math.ceil(Math.random() * (high - low) + low);
+	}
+	/*
 		* 重新布局所有图片
 		* @param centerIndex 指定居中排布哪个图片
 	*/
 	rearrange(centerIndex) {
+		let imgsArrangeArr = this.state.imgsArrangeArr,
+				Constant = this.state.Constant,
+				centerPos = Constant.centerPos,
+				hPosRange = Constant.hPosRange,
+				vPosRange = Constant.vPosRange,
+				hPosRangeLeftSecX = hPosRange.leftSecX,
+				hPosRangeRightSecX = hPosRange.rightSecX,
+				hPosRangeY = hPosRange.y,
+				vPosRangeX = vPosRange.x,
+				vPosRangeTopY = vPosRange.topY,
 
+				imgsArrangeTopArr = [],
+				topImgNum = Math.ceil(Math.random()*2),//取一个或不取
+				topImgSpliceIndex = 0,
+				imgsArrangeCenterArr = imgsArrangeArr.splice(centerIndex,1);
+
+				//首先居中 centerIndex 的图片
+				imgsArrangeCenterArr[0].pos = centerPos;
+
+				//取出要布局上侧的图片的状态信息
+				topImgSpliceIndex = Math.ceil(Math.random()*(imgsArrangeArr.length - topImgNum));
+				imgsArrangeTopArr = imgsArrangeArr.splice(topImgSpliceIndex, topImgNum);
+
+				//布局位于上侧的图片
+				imgsArrangeTopArr.forEach((value,index) => {
+					imgsArrangeTopArr[index].pos = {
+						top: this.getRangeRandom(vPosRangeTopY[0],vPosRangeTopY[1]),
+						left: this.getRangeRandom(vPosRangeX[0], vPosRangeX[1])
+					};
+				});
+				//布局左右两侧的图片
+				for (let i = 0,j = imgsArrangeArr.length,k = j/2;i < j;i++) {
+					let  hPosRangeLORX = null;
+					//前半部布局左边，右半部布局右边
+					if (i < k) {
+						hPosRangeLORX = hPosRangeLeftSecX;
+					} else {
+						hPosRangeLORX = hPosRangeRightSecX;
+					}
+
+					imgsArrangeArr[i].pos = {
+						top: this.getRangeRandom(hPosRangeY[0], hPosRangeY[1]),
+						left: this.getRangeRandom(hPosRangeLORX[0], hPosRangeLORX[1])
+					}
+				}
+
+				if (imgsArrangeTopArr && imgsArrangeTopArr[0]) {
+					imgsArrangeArr.splice(topImgSpliceIndex, 0, imgsArrangeTopArr[0]);
+				}
+
+				imgsArrangeArr.splice(centerIndex, 0, imgsArrangeCenterArr[0]);
+
+				//this.state.imgsArrangeArr = imgsArrangeArr;//Wrong : Do Not Modify State Directly
+				this.setState({
+					imgsArrangeArr: imgsArrangeArr
+				});
 	}
 	//组件加载以后，为每张图片计算其位置的范围
 	componentDidMount() {
@@ -104,8 +170,8 @@ class AppComponent extends React.Component {
 		//计算上侧区域图片排布位置的取值范围
 		this.state.Constant.vPosRange.topY[0] = -halfImgH;
 		this.state.Constant.vPosRange.topY[1] = halfStageH - halfImgH*3;
-		this.state.Constant.vPosRange.x[0] = halfImgW - imgW;
-		this.state.Constant.vPosRange.x[1] = halfImgW;
+		this.state.Constant.vPosRange.x[0] = halfStageW - imgW;
+		this.state.Constant.vPosRange.x[1] = halfStageW;
 
 		this.rearrange(0);
 	}
@@ -121,18 +187,17 @@ class AppComponent extends React.Component {
 				}
 			}
   		//imgFigures.push(<ImgFigure key={index.toString()} data={value} ref={'imgFigure'+index}/>);
-			imgFigures.push(<ImgFigure key={index.toString()} data={value} ref={(imgFigure) => { this.imgFigure = imgFigure; }}/>);
+			imgFigures.push(<ImgFigure key={index.toString()} data={value} ref={(imgFigure) => { this.imgFigure = imgFigure; }} arrange={this.state.imgsArrangeArr[index]}/>);
   	});
     return (
-	    // <section className="stage" ref="stage">
 			<section className="stage" ref={(stage) => { this.stageSec = stage; }}>
-	     	<section className="img-sec">
-	     		{imgFigures}
-	     	</section>
-	     	<nav className="controller-nav">
+				<section className="img-sec">
+					{imgFigures}
+				</section>
+				<nav className="controller-nav">
 					{controllerUnits}
-	     	</nav>
-	    </section>
+				</nav>
+			</section>
     );
   }
 }
